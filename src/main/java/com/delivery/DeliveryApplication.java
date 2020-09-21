acef7e004d6b9dcf0cf242175c8a707a7121d6d8
guthemberg.souza <guthemberg.souza@orubetecnologia.com.br>
2020-09-21 17:03:58 -0300

    ajuste de acesso diante requisicoes

--- src/main/java/com/delivery/DeliveryApplication.java
+++ src/main/java/com/delivery/DeliveryApplication.java
@@ -2,10 +2,19 @@ package com.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DeliveryApplication {
+@EnableWebMvc
+public class DeliveryApplication implements WebMvcConfigurer {
 
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
 	}
